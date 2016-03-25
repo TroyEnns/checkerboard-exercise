2d419cf033f9735198f53f6866a4ecc6e2c21428
Troy Enns <TroyEnns@gmail.com>
2016-03-25 16:31:22 -0700

    complete flashing

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ currentRow=1;
 var colors=[0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'];
 var currentColor="#";
 
+genDivs();
+
 function genDivs(){ 
       var e = document.body;  
       for(var i = 0; i < 7; i++){ 
@@ -32,13 +34,12 @@ function genDivs(){
         row.style.display="flex";
         e.appendChild(row);
         }
+        setTimeout(newColors,2000);
     }
-genDivs();
 
-
-function randomColor(){
-    for(i=0;i<6;i++){
-        currentColor+=colors[Math.floor(Math.random()*colors.length)];
+function newColors(){
+    while (document.body.firstChild) {
+    document.body.removeChild(document.body.firstChild);
     }
-    alert(currentColor);
+    genDivs();
 }
